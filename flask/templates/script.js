5bcedca0a45273c74b4e249a574f72de9580c6bc
harrykris42 <harrykris4274@gmail.com>
2024-02-14 13:06:39 +0530

    commit

--- flask/templates/script.js
+++
@@ -1,8 +0,0 @@
-function validateForm(){
-    var TN = document.getElementById('tn').value;
-    if (TN.trim()===""){
-        alert('Please enter a team name!');
-        return false;
-    }
-    return true;
-}
